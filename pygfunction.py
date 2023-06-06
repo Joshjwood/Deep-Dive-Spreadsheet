f73420a0ea1cd51a37f776c0edc6e8f2a911ce64
Josh <97046183+Joshjwood@users.noreply.github.com>
2023-06-06 16:35:54 +0100

    Add files via upload

---
+++ pygfunction.py
@@ -0,0 +1,24 @@
+import pygsheets
+
+'''
+took the below from here:
+https://stackoverflow.com/questions/62917910/how-can-i-export-pandas-dataframe-to-google-sheets-using-python
+'''
+
+
+
+def write_to_gsheet(service_file_path, spreadsheet_id, sheet_name, data_df):
+    """
+    this function takes data_df and writes it under spreadsheet_id
+    and sheet_name using your credentials under service_file_path
+    """
+    gc = pygsheets.authorize(service_file=service_file_path)
+    sh = gc.open_by_key(spreadsheet_id)
+    try:
+        sh.add_worksheet(sheet_name)
+    except:
+        pass
+    wks_write = sh.worksheet_by_title(sheet_name)
+    wks_write.clear('A1',None,'*')
+    wks_write.set_dataframe(data_df, (1,1), encoding='utf-8', fit=True)
+    wks_write.frozen_rows = 1
